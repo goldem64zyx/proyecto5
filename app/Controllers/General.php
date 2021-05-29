8b25d542aa1563a6f172ddf99a00fdc3b0054cbf
goldem64zyx <goldem64@gmail.com>
2021-05-29 08:47:19 -0500

    proyecto nuevo commit inicial

---
+++ app/Controllers/General.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Controllers;
+
+class General extends BaseController{
+
+    public function obtenerDatos(){
+
+    }
+
+    public function insertar(){
+
+    }
+
+    public function listarTodo(){
+
+    }
+
+    public function actualizar(){
+
+    }
+
+    public function eliminar(){
+        
+    }
+}
---
+++ app/Models/GeneralModel.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Models;
+use CodeIgniter\Model;
+class GeneralModel extends Model{
+    public function obtenerInformacion(){
+
+    }
+
+    public function listarTodo(){
+
+    }
+
+    public function insertar(){
+
+    }
+
+    public function actualizar(){
+
+    }
+
+    public function eliminar(){
+        
+    }
+
+}
+
